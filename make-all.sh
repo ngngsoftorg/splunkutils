a0813b939c67d528a5e22ac694efa2242ab3350f
ngngsoftorg <ng@ngsoft.org>
2012-12-12 18:00:29 -0800

    updating the make script to deploy default instead of local

--- make-all.sh
+++ make-all.sh
@@ -5,6 +5,6 @@ then
     echo "SPLUNK_HOME must be set."
 else
     cp -Rf bin/* $SPLUNK_HOME/etc/apps/importutil/bin/
-    cp -Rf local/* $SPLUNK_HOME/etc/apps/importutil/local/
+    cp -Rf default/* $SPLUNK_HOME/etc/apps/importutil/default/
     cp -Rf metadata/* $SPLUNK_HOME/etc/apps/importutil/metadata/
 fi
