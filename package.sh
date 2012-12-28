6838537ce194ac55e0c47c7b6530cc7b857a2012
ngngsoftorg <ng@ngsoft.org>
2012-12-28 15:25:50 -0800

    Removed ._* files from the tar Removed .DS_Store files from the tar

--- package.sh
+++ package.sh
@@ -2,4 +2,5 @@
 
 cd ~/sandbox
 rm importutil-$1.tar.gz
-tar cvzf importutil-$1.tar.gz --exclude="._*" --exclude=".git" --exclude="LINKS" --exclude="importtable.xcodeproj" --exclude="make*" --exclude="TODOS" --exclude="package.sh" --exclude="Crypto" --exclude="paramiko" importutil
+#The COPYFILE_DISABLE=1 is needed on a mac.
+COPYFILE_DISABLE=1 tar cvzf importutil-$1.tar.gz --exclude=".gitignore" --exclude=".DS_Store" --exclude=".git" --exclude="LINKS" --exclude="importtable.xcodeproj" --exclude="make*" --exclude="TODOS" --exclude="package.sh" --exclude="Crypto" --exclude="paramiko" importutil
